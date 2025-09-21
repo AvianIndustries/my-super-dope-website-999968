83c1a5c7c4ca02523ae55e297dd6d63757a12f02
AvianIndustries <62577392+AvianIndustries@users.noreply.github.com>
2025-09-21 13:56:44 -0500

    Add app/layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'My super dope website',
+  description: 'A modern marketing website: A flipping sick website about dogs',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
